111b792cdff3f665f7c14ee3e66f014416e22e01
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-10-13 08:32:10 -0300

    bugfix: adjustments to apply cached responses with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module, DynamicModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { CacheModule, CacheStoreFactory } from '@nestjs/common/cache';
+import { CacheModule, CacheStoreFactory } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CacheInterceptor } from '@nestjs/common/cache';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './models/user/user.service';
@@ -11,9 +11,25 @@ import { PrismaService } from './common/services/prisma.service';
 import { UserController } from './models/user/user.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      store: redisStore as unknown as CacheStoreFactory,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService, PrismaService],
+  providers: [
+    AppService,
+    UserService,
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {
   static forRoot(options: { db: number }): DynamicModule {
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule.forRoot({ db: 0 }));
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
